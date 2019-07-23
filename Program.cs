c06b8c4380858d3dad764c088d723b47f36d3f01
unknown <sales@beautyfloor.eu>
2019-07-23 20:26:34 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace FizzBuzz0704
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 21; i++)
+            {
+                //%dalījuma zīme bez atlikuma %
+
+                if (i % 5 == 0 && i % 3 == 0)
+
+                {
+                    //alternatīva,kā izdrukāt iegūtos lielumus
+                    //Console.WriteLine($"{i} FizzBuzz");
+                    PrintValue(i, "FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    //Console.WriteLine($"{i} Fizz");
+                    PrintValue(i, "Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    //Console.WriteLine($"{i} Buzz");
+                    PrintValue(i, "Buzz");
+                }
+                else
+                {
+                    //Console.WriteLine(i);
+                    PrintValue(i, "");
+                }
+
+
+
+
+
+
+            }
+        }
+
+        static void PrintValue(int i, string text)
+        {
+        Console.WriteLine($"{i} {text}");
+        }
+    }
+}
